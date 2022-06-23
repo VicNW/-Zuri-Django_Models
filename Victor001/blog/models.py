380c4329d393c4fa99f659ce892cd0b953da36b4
VicNW <nwosuvictor54@gmail.com>
2022-06-23 14:34:48 +0400

    I commented out foreign key to make the migrations

--- Victor001/blog/models.py
+++ Victor001/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     Title = models.CharField(max_length=100)
-    Text = models.TextField
-    Author = models.ForeignKey(get_user_model)()
+    Text = models.TextField()
+    # Author = models.ForeignKey(get_user_model)()
     Created_date = models.DateTimeField(auto_now_add=True)
     Published_date = models.DateTimeField
