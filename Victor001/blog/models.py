4d3c6c13f348bbaec72747842928d8ff42bc7c19
VicNW <nwosuvictor54@gmail.com>
2022-06-23 14:55:20 +0400

    I uncommented the Arthor fild in the models

--- Victor001/blog/models.py
+++ Victor001/blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Post(models.Model):
     Title = models.CharField(max_length=100)
     Text = models.TextField()
-    # Author = models.ForeignKey(get_user_model)()
+    Author = models.ForeignKey(get_user_model)()
     Created_date = models.DateTimeField(auto_now_add=True)
-    Published_date = models.DateTimeField
+    Published_date = models.DateTimeField()
